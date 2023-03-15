c5388effcb1cee7bea0e9aca968188841a18759d
Chris Lupton <42554017+clupt@users.noreply.github.com>
2023-03-14 20:10:07 -0500

    finished part one

--- outerspace.sql
+++ outerspace.sql
@@ -1,19 +1,22 @@
 CREATE TABLE stars(
-    star TEXT PRIMARY KEY, --primary key
-    temp INT NOT NULL);
+    star TEXT PRIMARY KEY, --primary key TODO: star_name
+    temp INT NOT NULL);-- TODO: temp_in_kelvin
 
 CREATE TABLE planets(
     planet_name TEXT PRIMARY KEY,
-    orbital_period FLOAT NOT NULL,
-    star_name TEXT NOT NULL REFERENCES stars); -- foreign key
+    orbital_period FLOAT NOT NULL, --TODO: orbital_period_in_years
+    star_name TEXT NOT NULL REFERENCES stars); -- foreign key --TODO: change star_name to star
 
 CREATE TABLE moons(
     moon TEXT PRIMARY KEY,
     planet TEXT NOT NULL REFERENCES planets); -- foreign key
 
-INSERT into stars (star, temp) VALUES ('Proxima Centauri', 3042);
-INSERT into stars (star, temp) VALUES ('Gliese 876', 3192);
-INSERT into stars (star, temp) VALUES ('The Sun', 5800);
+INSERT into stars (star, temp)
+    VALUES
+    ('Proxima Centauri', 3042),
+    ('Gliese 876', 3192),
+    ('The Sun', 5800);
+
 
 INSERT into planets (planet_name, orbital_period, star_name) VALUES ('Earth', 1.00, 'The Sun');
 INSERT into planets (planet_name, orbital_period, star_name) VALUES ('Mars', 1.882, 'The Sun');
